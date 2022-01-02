e51c17bff6da249f461305f1cecc198b7703030e
Akinwole Diekololaoluwa <91050325+khaldieko@users.noreply.github.com>
2022-01-02 15:17:26 +0100

    good work

---
+++ src/Middleware/timecheck.js
@@ -0,0 +1,19 @@
+function timecheck(req, res, next) {
+  var validDays = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri"];
+  var validEntryDate = 9;
+
+  var validCloseDate = 17;
+
+  var currentDate = Date().split(" ");
+  var currentTime = currentDate[4].split(":");
+  if (validDays.includes(currentDate[0])&&(Number(currentTime[0]>=validEntryDate&&currentTime[0]<=validCloseDate))) {
+    next();
+  } else {
+     return res.send({
+          message: "The website is closed at this hour"
+      })
+  }
+  
+  
+}
+export default timecheck;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import path from "path"
+import timecheck from "./Middleware/timecheck.js";
 
 const app = express();
 const PORT = 8000;
@@ -9,11 +11,20 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    status: "success",
-    message: "hello world",
-  });
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const options= path.join(__dirname)
+
+app.get("/", timecheck, (req, res) => {
+  res.sendFile(path.resolve(options, "./Public/Home.html"))
+});
+
+app.get("/ContactUs", (req, res) => {
+  res.sendFile(path.resolve(options, "./Public/ContactUs.html"))
+});
+
+app.get("/OurServices", (req, res) => {
+  res.sendFile(path.resolve(options, "./Public/OurServices.html"))
 });
 
 app.listen(PORT, () => {
